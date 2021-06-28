eb4b5b03f09787963ce972843ab8f5906d8b7e8f
Bieliaiev Vlas <bieliaiev.vlas@example.com>
2021-06-28 05:44:37 -0900

    case-classes-INIT

--- src/main/scala/org/example/Main.scala
+++ src/main/scala/org/example/Main.scala
@@ -1,5 +1,27 @@
 package main.scala.org.example
 
-object Main extends App {
+object Main {
 
+  def main(args: Array[String]): Unit = {
+
+    val message1 = Message("jorge@catalonia.es", "guillaume@quebec.ca", "Com va?")
+    val message2 = Message("jorge@catalonia.es", "guillaume@quebec.ca", "Com va?")
+
+    //equality
+    assert(message1 == message2)
+
+    //copying
+    val message5 = message2.copy(recipient = "claire@bourgogne.fr")
+    println(message5)
+  }
 }
+
+/**
+ *
+ * case classes equality compares by structure, not by hrefs
+ *
+ * @param sender    public and immutable string value
+ * @param recipient public and immutable string value
+ * @param body      public and immutable string value
+ */
+case class Message(sender: String, recipient: String, body: String)
