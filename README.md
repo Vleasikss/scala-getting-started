ccc745b70928b3771672afebd2ea5975df6dfbb0
Bieliaiev Vlas <bieliaiev.vlas@example.com>
2021-06-28 02:26:20 -0900

    created base-types documentation

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
+## Any
+
+- is a supertype of all the objects
+- contains equals, hashcode, toString methods
+- almost similiar to 'Object.class' in java
+
+## AnyVal
+
+- is a subtype of 'Any'
+- contains all the primitive types in java (Double, Float, Long, Int, Short, Byte, Char, Unit, Boolean)
+- the values can't be null
+
+## AnyRef
+
+- is a subtype of 'Any'
+- contains all the others objects in Java (* that contains hrefs on stack on heap)
+- similar to java.lang.Object in java
+
+
+
 sbt compile -> compile the project
 
 sbt run -> run the project
--- src/main/scala/org/example/Main.scala
+++ src/main/scala/org/example/Main.scala
@@ -1,38 +1,43 @@
 package main.scala.org.example
 
-object Main extends App {
+/**
+ * Type Any -
+ */
+object Main {
 
-  /**
-   * The last command is a returned value
-   * [That's just like in Ruby language]
-   *
-   * @param input - number to square
-   */
-  def getSquareString(input: Double): Double = {
-    val square = input * input
-    square
+  val ANY_VALUES: Map[String, AnyVal] = Map(
+    "Integer" -> 123,
+    "Char" -> 'c',
+    "Float" -> 123.123,
+    "Boolean" -> true
+  )
+  val ANY_REFS: Map[String, AnyRef] = Map(
+    "Lambda" -> (() => "Lambda function"),
+    "List" -> List("STRING"),
+    "String" -> "String"
+  )
+
+  def main(args: Array[String]): Unit = {
+    val any: List[Any] = List(
+      ANY_VALUES("Integer"),
+      ANY_VALUES("Char"),
+      ANY_VALUES("Boolean"),
+      ANY_REFS("String"),
+      ANY_REFS("Lambda")
+    )
+    any.foreach(element => println(element))
+
+    val listValues: List[AnyVal] = List(
+      ANY_VALUES("Integer"),
+      ANY_VALUES("Float"),
+      ANY_VALUES("Boolean"))
+    listValues.foreach(element => println(element))
+
+    val listObjects: List[AnyRef] = List(
+      ANY_REFS("Lambda"),
+      ANY_REFS("String"),
+      ANY_REFS("List")
+    )
+    listObjects.foreach(element => println(element))
   }
-
-  /**
-   * Default lambda expression
-   */
-  val addOne: Int => Int = (x: Int) => x + 1
-
-
-  /**
-   * Method that contains list of params
-   *
-   * @param x          first arg of first params list
-   * @param y          second arg of first params list
-   * @param multiplier first arg of second params list
-   */
-  def addThenMultiply(x: Int, y: Int)(multiplier: Int): Int = (x + y) * multiplier
-
-
-  val square = getSquareString(12)
-  assert(square == 144.0)
-
-  val i: Int = addOne(10)
-  assert(i == 11)
-
 }
