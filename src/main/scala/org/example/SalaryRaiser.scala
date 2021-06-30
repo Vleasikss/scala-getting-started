b7786f94d4f3ccfe84249ca347652b17521e1f83
Vlas <42597604+Vleasikss@users.noreply.github.com>
2021-06-29 23:57:49 -0900

    refactoring

--- src/main/scala/org/example/SalaryRaiser.scala
+++ src/main/scala/org/example/SalaryRaiser.scala
@@ -2,15 +2,18 @@ package main.scala.org.example
 
 object SalaryRaiser {
 
+  val SMALL_PROMOTION_PERCENTAGE = 1.1
+  val BIG_PROMOTION_PERCENTAGE = 1.5
+
   private def promotion(salaries: Seq[Double], promotionFunction: Double => Double): Seq[Double] = {
     salaries.map(promotionFunction)
   }
 
   def smallPromotion(salaries: Seq[Double]): Seq[Double] =
-    promotion(salaries, salary => salary * 1.1)
+    promotion(salaries, salary => salary * SMALL_PROMOTION_PERCENTAGE)
 
   def bigPromotion(salaries: Seq[Double]): Seq[Double] =
-    promotion(salaries, salary => salary * 1.5)
+    promotion(salaries, salary => salary * BIG_PROMOTION_PERCENTAGE)
 
   def hugePromotion(salaries: Seq[Double]): Seq[Double] =
     promotion(salaries, salary => salary * salary)
