6cfa931068afc48ba7a09d26797a4dea3e199427
Vlas <42597604+Vleasikss@users.noreply.github.com>
2021-07-08 11:51:41 +0300

    docs added

--- src/main/scala/org/example/Main.scala
+++ src/main/scala/org/example/Main.scala
@@ -2,6 +2,12 @@ package org.example
 
 /**
  * Примеси (Mixin) - это трейты, которые используются для создания класса.
+ *
+ * Difference between inheritance:
+ *  - mixins is more about dynamically adding a set of methods into an object
+ *
+ *  1. Inheritance used for inheriting properties
+ *  1. Mixin used for inheriting functionality
  */
 object Main {
 
